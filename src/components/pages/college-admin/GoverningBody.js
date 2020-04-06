ffa46c1e4540650ceeed33af058de7a9098bb52a
Kishor <kctripathy@gmail.com>
2020-04-06 11:50:10 +0530

    Added for governing body

--- src/components/pages/college-admin/GoverningBody.js
+++ src/components/pages/college-admin/GoverningBody.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import WorkinProgress from '../WorkInProgress';
+import Layout from '../Layout';
 
 export default function GoverningBody() {
     return (
-        <>
+        <Layout title="Governing Body of the College">
             <WorkinProgress />
-        </>
+        </Layout>
     );
 }
